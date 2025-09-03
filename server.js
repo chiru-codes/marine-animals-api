e9ed3ebac6c4eaac6ce43495f35ec5415e32997b
Ela <miruxchiru@gmail.com>
2025-09-03 01:52:37 -0500

    fix(server): Fixed server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const marineRouter = require('./routes/marineAnimals');
 
 app.use('/api/marine', marineRouter);
 
-app.listen(port, () => {
-  console.log(`API siendo llamada en http://localhost:${port}/api/marine`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`API de animales marinos escuchando en http://0.0.0.0:${port}/api/marine`);
 });
